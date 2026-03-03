1be13f5212bf19c0eedc7962bac9eca40767a6ce
Jason Beach <15958424+IMTorg@users.noreply.github.com>
2026-03-02 22:13:29 -0500

    add moments to Metric class

--- macroecon_wrappy/metric.py
+++ macroecon_wrappy/metric.py
@@ -12,6 +12,7 @@ __license__ = "MIT"
 
 
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import normalize
 
 from copy import deepcopy
@@ -91,14 +92,124 @@ class Metric(pd.Series):
         """Get metadata."""
         return self._metadata
     
+    def change_time(self, rule='M'):
+        """Change time frequency
+
+        rules:
+            * T or min: Minute
+            * H: Hour
+            * D: Day
+            * B: Business day
+            * W: Week
+            * M: Month end
+            * MS: Month start
+            * Q: Quarter end
+            * A or Y: Year end 
+
+        Key Parameters
+            * rule: The frequency string (e.g.'5min''2S').
+            * closed: Which side of the interval is closed ('left' or 'right').
+            * label: Which bin edge label to use ('left' or 'right').
+        TODO: use mean or closing?
+        """
+        result  = self.resample(rule).mean()
+        result = Metric(result)
+        return result 
+
     def normalize_values(self, fun=None):
-        """Apply normalization function and get deep, new Metric."""
+        """Apply normalization function and get new (deep copy) Metric.
+        
+        """
         def minmax(vec):
             return (vec-vec.min())/(vec.max()-vec.min())
 
         if not fun:
             fun = minmax
-        metric = deepcopy(self)
-        new_metric = Metric(fun(metric))
-        return new_metric
+        result = Metric(fun(self))
+        return result
+    
+    def returns(self, type='simple', days=252):
+        """
+        Get a variety of metric returns
         
+        :param type: type of returns requested, in 'simple', 'cumulative', 'log', 'annualized'
+        :param days: (trading days) typically used to annualize return when metric values are daily
+        """
+        type_options = ['simple', 'cumulative', 'log', 'annualized']
+        result = None
+        timeframe_return = self.pct_change()   #typically used with `data['Adj Close']` 
+        if type==type_options[0]:
+            result = timeframe_return
+        elif type==type_options[1]:
+            cumulative_return = (1 + timeframe_return).cumprod()
+            result = cumulative_return
+        elif type==type_options[2]:
+            log_return = np.log(self.values / self.shift(1))
+            result = log_return
+        elif type==type_options[3]:
+            annual_return = timeframe_return.mean() * days
+            result = annual_return
+        result = Metric(result)
+        return result
+    
+    def ema(self, span=20, adjust=False):
+        """
+        Calculate EMA using pandas ewm()
+        Note:
+            typically one usses 'Close'
+            span: corresponds to the N-day EMA
+            adjust=False: ensures the formula uses the recursive formula: EMA_t = alpha*x_t + (1-alpha)*EMA_{t-1}
+        
+        Key Considerations
+            * EMA Formula: The .ewm() method uses the formula, above 
+            * Adjust Parameter: Using adjust=False provides a more consistent, recursive EMA calculation, which is preferred for technical analysis.
+            * Window Size: Common EMAs include 9, 12, 26, 50, and 200 days.
+            * Handling NaNs: The first few values will be equivalent to a Simple Moving Average (SMA) or NaN depending on how the calculation is initialized, as it needs previous data to establish the first EMA point. 
+        """
+        ema = self.ewm(span=span, adjust=adjust).mean()
+        result = Metric(ema)
+        return result
+
+    def moment_values(self, type='EMA', window=21, trading_days=252):
+        """
+        Get metric's moment values, such as
+        * Exponential Moving Avg
+        * Volatility
+        * etc.
+
+        :param type <'VOL','SKW','KUR'>: 
+        :param trading_days=252 annualizes the values
+
+        Usage
+            * Log returns with `metric1.returns(type='log')` are preferred for volatility because they are additive
+            * Often use 21-day (approx. one trading month) window as an example for monthly volatility
+            * 252 is the typical number of trading days in a year
+        """
+        type_mapping = {
+            'VOL':{
+                'type':'volatility',
+                'function': 'std',
+                'exponent': 1/2,
+            },
+            'SKW':{
+                'type':'skew',
+                'function': 'skew',
+                'exponent': 1/3,
+            },
+            'KUR':{
+                'type':'kurtosis',
+                'function': 'kurtosis',
+                'exponent': 1/4,
+            },
+        }
+        metric = self
+        if trading_days:
+            if len(metric) < trading_days:
+                return None
+        else:
+            trading_days = self.shape[0]
+        func = type_mapping[type]['function']
+        exponent = type_mapping[type]['exponent']
+        result = getattr(metric.rolling(window=window), func)() * np.power(trading_days, exponent)
+        result = Metric(result)
+        return result
--- tests/test_metric.py
+++ tests/test_metric.py
@@ -12,10 +12,16 @@ from macroecon_wrappy.metric import Metric
 from tests.data.test_data import df_series
 
 import pandas as pd
+import numpy as np
 
 
 
-def test_metric():
+
+
+
+
+
+def test_metric_basic():
     ts = df_series['value1']
     metric1 = Metric(ts)
     metric1.set_metadata(title='title1')
@@ -31,3 +37,27 @@ def test_metric():
     #metric attributes
     assert metric1.title == 'title1'
     assert metric1.get_metadata()['title'] == 'title1'
+
+def test_metric_advanced():
+    date_range = pd.date_range(start='2023-01-01', periods=72, freq='H')   #define a date range (e.g., hourly data for 72 periods)
+    np.random.seed(42)
+    random_values = np.abs(np.round(np.random.randn(len(date_range)), decimals=2))    #generate random data for the values (e.g., using numpy.random.randn)
+    ts = pd.Series(random_values, index=date_range)
+    metric1 = Metric(ts)
+    metric1.set_metadata(title='title1')
+    #time change
+    daily_metric = metric1.change_time(rule='D')
+    daily_metric_rounded = np.round(daily_metric.tolist(), decimals=2)
+    assert daily_metric_rounded.tolist() == [0.78, 0.72, 0.74]
+    #returns
+    cumulative_returns = metric1.returns(type='cumulative')
+    cumulative_returns_head = np.round(cumulative_returns, decimals=2).tolist()[-4:]
+    assert cumulative_returns_head == [0.72, 1.3, 0.72, 3.08]
+    #exponential moving average
+    ema = metric1.ema(span=20)
+    ema_head = np.round(ema.tolist()[:4],decimals=2).tolist()
+    assert ema_head == [0.5, 0.47, 0.48, 0.58]
+    #moments
+    historical_volitility = metric1.returns(type='log').moment_values(type='VOL', trading_days=70)
+    historical_volitility_head = np.round(historical_volitility, decimals=2).tolist()[-4:]
+    assert historical_volitility_head == [10.5, 10.35, 9.93, 9.79]
